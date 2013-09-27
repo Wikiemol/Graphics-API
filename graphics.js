67e18bbc418c6565571a649e7cd49eb63f526999
William <Kingweko@gmail.com>
2013-09-27 15:04:20 -0400

    making drawLine more streamlined, fixing lots of bugse

--- graphics.js
+++ graphics.js
@@ -1,87 +1,80 @@
-function Graphics(context){
+function Graphics(context,c){
 	this.cxt = context;
-	//this.cxt.fillRect(0,0,100,100);
-	
-	
-	
+	var color = c;
+	this.setColor = function(cl){
+		color = cl;
+	}
+	this.getColor = function(){
+		return color;
+	}
 }
 
-Graphics.prototype.drawPixel = function(x,y,color){
-	this.cxt.fillStyle = color;
+Graphics.prototype.drawPixel = function(x,y){
+	this.cxt.fillStyle = this.getColor();
 	this.cxt.fillRect(x,y,1,1);
+	
 }
 
-Graphics.prototype.drawLine = function(x1,y1,x2,y2,color){
+Graphics.prototype.drawLine = function(x_1,y_1,x_2,y_2){
+	var x1 = x_1;
+	var x2 = x_2;
+	var y1 = y_1;
+	var y2 = y_2;
 	
 	var dx = x2 - x1;
 	var dy = y2 - y1;
 	
-	var s = 0;
-	var dist = Math.sqrt(dx*dx + dy*dy);
-	var g = new Graphics(this.cxt);
+	var slope = 0;
+	
 	if(dx == 0 && dy == 0){
-		g.drawPixel(x1,y1,color);
+		
+		this.drawPixel(x1,y1);
 		return;
+	}else if(dx == 0 && dy != 0){
+		
+		for(var i = 0; i < Math.abs(dy); i++){
+			this.drawPixel(x1,y1+i*dy/Math.abs(dy));
+		}
+		return;
+	}else if(dx != 0 && dy == 0){
+		for(var i = 0; i < Math.abs(dx); i++){
+			this.drawPixel(x1+i*dx/Math.abs(dx),y1);
+		}
+		return;
+	}else{}
+	
+	if(dx < 0 && dy < 0){
+		x1 = x_2;
+		y1 = y_2;
+		x2 = x_1;
+		y1 = y_2;
+		
 	}
 	
-	if(Math.abs(dx) > Math.abs(dy)){
+	if(dy >= dx){
 		
-		s = Math.min(Math.abs(Math.floor(dx/dy) - (dx/dy)),Math.abs(Math.ceil(dx/dy) - (dx/dy)));
-		if(s==Math.abs(Math.floor(dx/dy) - (dx/dy))){
-			
-			s = Math.floor(dx/dy);
-		}
-		if(s==Math.abs(Math.ceil(dx/dy) - (dx/dy))){
-			
-			s = Math.ceil(dx/dy);
-		}
+		slope = dy/dx;
 		
-		for(var i = 0; i < Math.abs(dx/s); i++){
-			g.drawPixel(x1+i,y1 +Math.floor(i/s),color);
-		}
-		
-	}else if(dx == dy){
-		s = 1;
-		for(var i = 0; i < dx; i++){
-			console.log(dx + " " + (dx/s));
-			g.drawPixel(x1+i,y1+i,color);
-			
+		for(var i = 0; i < Math.abs(dy);i++){
+			this.drawPixel(x1+Math.floor(i/slope),y1+i);
+			//console.log((x1+Math.floor(i/slope)) + " " + (y1 + i*s/Math.abs(s)))
 		}
 	}else{
-		s = Math.min(Math.abs(Math.floor(dy/dx) - (dy/dx)),Math.abs(Math.ceil(dy/dx) - (dy/dx)));
-		if(s==Math.abs(Math.floor(dy/dx) - (dy/dx))){
-			s = Math.floor(dy/dx);
-		}
-		if(s==Math.abs(Math.ceil(dy/dx) - (dy/dx))){
-			s = Math.ceil(dy/dx);
-		}
-		for(var i = 0; i < Math.abs(dy/s); i++){
-				g.drawPixel(x1 + Math.floor(i/s), y1+i,color);
+		slope = dx/dy;
+		
+		console.log("Slope: " + dx/dy);
+		
+		for(var i = 0; i < Math.abs(dx); i++){
 			
+			this.drawPixel(x1+i,y1+Math.floor(i/slope));
+			console.log((x1+i*slope/Math.abs(slope)) + " " + (y1+Math.floor(i/slope)));
 		}
 	}
 
-	function gcf(alp,bet){
-		
-		var a = alp;
-		var b = bet;
-		var r = 0;
-		if(a<b){
-			a = bet;
-			b = alp;
-		}
-		
-		r = a%b;
 
-		if(r != 0){
-			return gcf(a,r);
-		}
-			
-		return b;
-	
-		
-		
-		
-	}
 }
+
+
+
+
 
