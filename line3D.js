e5b05e3c46042601ccf480dedbe7bf43a3d944a2
William <Kingweko@gmail.com>
2013-11-16 18:18:54 -0500

    mid in triangle defined

--- line3D.js
+++ line3D.js
@@ -2,13 +2,11 @@ function Line3D(p1,p2,material){
 	this.p1 = p1;
 	this.p2 = p2;
 	this.material = material;
+	this.mid 	= this.p1.add(this.p2).multiply(1/2);
 }
 
 Line3D.prototype.midPoint = function() {
-	var midPoint = new Vector(0,0,0);
-	midPoint = midPoint.add(this.p1.multiply(1/2));
-	midPoint = midPoint.add(this.p2.multiply(1/2));
-
+	var midPoint = this.p1.add(this.p2).multiply(1/2);
 	return midPoint;
 };
 
--- triangle3D.js
+++ triangle3D.js
@@ -1,22 +1,30 @@
 function Triangle3D(p1,p2,p3,m){ //position vectors as first three parameters and material
-	this.p1 = p1;
-	this.p2 = p2;
-	this.p3 = p3;
-	this.material = m;
+	this.p1 		= p1;
+	this.p2 		= p2;
+	this.p3 		= p3;
+	this.material 	= m;
+	this.normal1 	= this.normal(); //normal for vertex 1
+	this.normal2 	= this.normal(); //normal for vertex 2
+	this.normal3 	= this.normal(); //normal for vertex 3
+	this.flip 		= false;
+	this.mid 		= this.p1.add(this.p2).add(this.p3).multiply(1/3);
+	
 }
 
 Triangle3D.prototype.midPoint = function() {
-	var midPoint = this.p1.multiply(1/3);
-	midPoint = midPoint.add(this.p2.multiply(1/3));
-	midPoint = midPoint.add(this.p3.multiply(1/3));
-	
+	var midPoint = this.p1.add(this.p2).add(this.p3).multiply(1/3);
 	return midPoint;
 };
 
 Triangle3D.prototype.normal = function(){
 	var v1 = this.p1.subtract(this.p2);
 	var v2 = this.p3.subtract(this.p2);
-	return v1.cross(v2);
+	if(this.flip){
+		return v1.cross(v2).multiply(-1);
+	}else{
+		return v1.cross(v2);
+	}
+	
 }
 
 Triangle3D.prototype.getMaterial = function() {
